dae57b9bfd303829236293e1348f5a2a353e9d63
Oleg Prans <oleg@prans.net>
2012-12-31 11:58:55 -0500

    temporary drop label

--- ajax_upload/static/ajax_upload/js/ajax-upload-widget.js
+++ ajax_upload/static/ajax_upload/js/ajax-upload-widget.js
@@ -3,7 +3,7 @@
         var global = this;
         //var $ = global.$;
         var console = global.console || {log: function() {}};
-    
+
         var AjaxUploadWidget = global.AjaxUploadWidget = function(element, options) {
             this.options = {
                 changeButtonText: 'Change',
@@ -19,29 +19,29 @@
             this.$element = $(element);
             this.initialize();
         };
-    
+
         AjaxUploadWidget.prototype.DjangoAjaxUploadError = function(message) {
             this.name = 'DjangoAjaxUploadError';
             this.message = message;
         };
         AjaxUploadWidget.prototype.DjangoAjaxUploadError.prototype = new Error();
         AjaxUploadWidget.prototype.DjangoAjaxUploadError.prototype.constructor = AjaxUploadWidget.prototype.DjangoAjaxUploadError;
-    
+
         AjaxUploadWidget.prototype.initialize = function() {
             var self = this;
             this.name = this.$element.attr('name');
-    
+
             // Create a hidden field to contain our uploaded file name
             this.$hiddenElement = $('<input type="hidden"/>')
                 .attr('name', this.name)
                 .val(this.$element.data('filename'));
             this.$element.attr('name', ''); // because we don't want to conflict with our hidden field
             this.$element.after(this.$hiddenElement);
-    
+
             // Initialize preview area and action buttons
             this.$previewArea = $('<div class="'+this.options.previewAreaClass+'"></div>');
             this.$element.before(this.$previewArea);
-    
+
             // Listen for when a file is selected, and perform upload
             this.$element.on('change', function(evt) {
                 self.upload();
@@ -53,7 +53,7 @@
                     $(this).hide();
                 });
             this.$element.after(this.$changeButton);
-    
+
             this.$removeButton = $('<button type="button" class="btn-remove"></button>')
                 .text(this.options.removeButtonText)
                 .on('click', function(evt) {
@@ -65,10 +65,10 @@
                     self.displaySelection();
                 });
             this.$changeButton.after(this.$removeButton);
-    
+
             this.displaySelection();
         };
-    
+
         AjaxUploadWidget.prototype.upload = function() {
             var self = this;
             if(!this.$element.val()) return;
@@ -87,7 +87,7 @@
                 error: function(data) { self.uploadFail(data); }
             });
         };
-    
+
         AjaxUploadWidget.prototype.uploadDone = function(data) {
             // This handles errors as well because iframe transport does not
             // distinguish between 200 response and other errors
@@ -107,7 +107,7 @@
                 if(this.options.onComplete) this.options.onComplete.call(this, data.path);
             }
         };
-    
+
         AjaxUploadWidget.prototype.uploadFail = function(xhr) {
             if(this.options.onError) {
                 this.options.onError.call(this);
@@ -116,14 +116,14 @@
                 console.log(xhr);
             }
         };
-    
+
         AjaxUploadWidget.prototype.displaySelection = function() {
             var filename = this.$hiddenElement.val();
-    
+
             if(filename !== '') {
                 this.$previewArea.empty();
                 this.$previewArea.append(this.generateFilePreview(filename));
-    
+
                 this.$previewArea.show();
                 this.$changeButton.show();
                 if(this.$element.data('required') === 'True') {
@@ -139,10 +139,10 @@
                 this.$element.show();
             }
         };
-    
+
         AjaxUploadWidget.prototype.generateFilePreview = function(filename) {
             // Returns the html output for displaying the given uploaded filename to the user.
-            var prettyFilename = this.prettifyFilename(filename);
+            var prettyFilename = "";//this.prettifyFilename(filename);
             var output = '<a href="'+filename+'" target="_blank">'+prettyFilename+'';
             $.each(['jpg', 'jpeg', 'png', 'gif'], function(i, ext) {
                 if(filename.toLowerCase().slice(-ext.length) == ext) {
@@ -153,15 +153,15 @@
             output += '</a>';
             return output;
         };
-    
+
         AjaxUploadWidget.prototype.prettifyFilename = function(filename) {
             // Get rid of the folder names
             var cleaned = filename.slice(filename.lastIndexOf('/')+1);
-    
+
             // Strip the random hex in the filename inserted by the backend (if present)
             var re = /^[a-f0-9]{32}\-/i;
             cleaned = cleaned.replace(re, '');
-    
+
             // Truncate the filename
             var maxChars = this.options.previewFilenameLength;
             var elipsis = '...';
@@ -170,7 +170,7 @@
             }
             return cleaned;
         };
-    
+
         AjaxUploadWidget.autoDiscover = function(options) {
             $('input[type="file"].ajax-upload').each(function(index, element) {
                 new AjaxUploadWidget(element, options);
